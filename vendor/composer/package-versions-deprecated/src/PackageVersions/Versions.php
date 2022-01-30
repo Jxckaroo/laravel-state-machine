6e41d0ef24f1ffa942317d88cdab5ed14f1bc39b
Jxckaroo <Jxckaroo@users.noreply.github.com>
2022-01-30 23:09:05 +0000

    Apply php-cs-fixer changes

--- vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
+++ vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
@@ -164,7 +164,7 @@ final class Versions
   'zbateson/mail-mime-parser' => '2.2.0@038d5043a54ee198ed96ab5fd39f16231272b32e',
   'zbateson/mb-wrapper' => '1.1.1@bfd45fb3e2a8cf4c496b2c3ebd63b5f815721498',
   'zbateson/stream-decorators' => '1.0.6@3403c4323bd1cd15fe54348b031b26b064c706af',
-  'jxckaroo/laravel-state-machine' => 'dev-develop@cbf6a01c710069d2276a024ebf1f2d560ffd4de6',
+  'jxckaroo/laravel-state-machine' => 'dev-develop@417e4d104447e824adc99c1081a8053863049c09',
 );
 
     private function __construct()
